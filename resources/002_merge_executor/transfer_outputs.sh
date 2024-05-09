85d2609908d7f91ad2e0bb07beea5a55f7a7ada6
Alvaro Vidal Torreira <avidalto@hawk.iit.edu>
2024-05-09 06:18:51 -0500

    Update transfer_outputs.sh

--- resources/002_merge_executor/transfer_outputs.sh
+++ resources/002_merge_executor/transfer_outputs.sh
@@ -3,7 +3,7 @@
 
 # Copy path/to/simulation_<i> to bucket
 # Delete unmerged results
-rm Results/${in_name}_*.*
+rm -f Results/${in_name}_*.hst Results/${in_name}_*.hlm
 
 aws s3 cp --recursive Results s3://$BUCKET_NAME/${dcs_output_directory}/${USER}/${workflow_name}/${job_number}/Results
 aws s3 cp --recursive TempData s3://$BUCKET_NAME/${dcs_output_directory}/${USER}/${workflow_name}/${job_number}/TempData
