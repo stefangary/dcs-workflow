6eb0f4828730c5dabd34c37f20fe0635b94e0c3c
avidalto <avidalto@hawk.iit.edu>
2024-06-24 04:57:53 -0500

    Report usage on simulation executor

--- resources/001_simulation_executor/run_dcs.sh
+++ resources/001_simulation_executor/run_dcs.sh
@@ -6,9 +6,25 @@ if ! [ -z "${dcs_load}" ]; then
     eval "${dcs_load}"
 fi
 
+# Create metering script
+cat >> metering.sh <<HERE
+#!/bin/bash
+metering_filename=$(hostname)-${job_number}
+while true; do
+    ssh ${resource_ssh_usercontainer_options} usercontainer ssh ${metering_user}@${metering_ip} "date" >> ~/.3dcs/usage-pending/
+    sleep 60
+done
+HERE
+
+chmod + metering.sh
+./metering.sh &
+metering_pid=$!
+
 # Run 3dcs
 SECONDS=0
 eval "${dcs_run}"  macroScript.txt
+kill ${metering_pid}
+
 # Results is own by root
 sudo chmod 777 Results/ -R
 echo ${SECONDS} > Results/dcs-runtime_${case_index}.txt
