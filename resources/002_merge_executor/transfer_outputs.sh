98ffd2acc9a90704d3a4460c23a85c01cd9f574f
avidalto <avidalto@hawk.iit.edu>
2024-07-01 09:43:22 -0500

    Save merge.sh and do not run merge job if only 1 worker is selected

--- resources/002_merge_executor/transfer_outputs.sh
+++ resources/002_merge_executor/transfer_outputs.sh
@@ -1,5 +1,6 @@
 # HARDCODED TO AWS
 # A dynamicStorage parameter type would be very helpful for this
 
+aws s3 cp --recursive merge.sh s3://$BUCKET_NAME/${dcs_output_directory}/${USER}/${workflow_name}/${job_number}/merge.sh
 aws s3 cp --recursive Results s3://$BUCKET_NAME/${dcs_output_directory}/${USER}/${workflow_name}/${job_number}/Results
 aws s3 cp --recursive TempData s3://$BUCKET_NAME/${dcs_output_directory}/${USER}/${workflow_name}/${job_number}/TempData
