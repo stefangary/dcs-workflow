bfa5f850f6954089e1e2be6235ce398fe0ddca34
avidalto <avidalto@hawk.iit.edu>
2024-08-09 10:26:07 -0500

    Add ssh options

--- resources/001_simulation_executor/run_dcs.sh
+++ resources/001_simulation_executor/run_dcs.sh
@@ -5,7 +5,7 @@
 cat >> metering.sh <<HERE
 #!/bin/bash
 while true; do
-    ssh -J ${resource_privateIp} ${metering_user}@${metering_ip} \"date >> /home/${metering_user}/.3dcs/usage-pending/$(hostname)-${job_number}\"
+    ssh -J ${resource_privateIp} -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ${metering_user}@${metering_ip} \"date >> /home/${metering_user}/.3dcs/usage-pending/$(hostname)-${job_number}\"
     if [ \$? -ne 0 ]; then
         echo "Unable to report usage to ${metering_user}@${metering_ip}. Killing Slurm job."
         scancel ${SLURM_JOB_ID}
